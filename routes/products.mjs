e2792bcdb28fa3b00fd45483f2775c3d83445684
Anderson Mendes Barros <pe22gug@eduvaud.ch>
2024-12-06 15:15:54 +0100

    premier commit

---
+++ routes/products.mjs
@@ -0,0 +1,11 @@
+import express from "express";
+import { products } from "../db/mock-product.mjs";
+import { success } from "./helper.mjs";
+
+
+const productsRouter = express();
+productsRouter.get("/", (req, res) => {
+    const message = "La liste des produits a bien été récupérée.";
+    res.json(success(message, products));
+});
+export { productsRouter };
