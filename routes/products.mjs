3414b04453f13edf5c4579fccec09448f256ccf0
Anderson Mendes Barros <pe22gug@eduvaud.ch>
2024-12-19 09:06:29 +0100

    corr test formatif

--- routes/products.mjs
+++ routes/products.mjs
@@ -4,8 +4,19 @@ import { success } from "./helper.mjs";
 
 
 const productsRouter = express();
-productsRouter.get("/", (req, res) => {
+/*productsRouter.get("/", (req, res) => {
     const message = "La liste des produits a bien été récupérée.";
     res.json(success(message, products));
+});*/
+
+productsRouter.get("/", (req, res) => {
+    const message = `La liste des ${products.length} produits a bien été récupérée.`;
+    res.json(success(message, products));
 });
+
+productsRouter.get("/filter", (req, res) => {
+    const message = "Le compteurs des produits filtrés a bien été récupéré.";
+    res.json(success(message, products.filter(x => x.price > 2.99)));
+});
+
 export { productsRouter };
--- src/app.mjs
+++ src/app.mjs
@@ -1,11 +1,15 @@
 import express from "express";
-
-
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("API REST of self service machine !");
 });
+app.get("/api/", (req, res) => {
+    res.redirect(`http://localhost:${port}/`);
+});
+import { productsRouter } from "../routes/products.mjs";
+app.use("/api/products", productsRouter);
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
 });
+
